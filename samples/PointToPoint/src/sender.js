4b545e7b2434dabff06e51b639fd97d18012c7dd
Tim Watson <twatson@ruffer.co.uk>
2015-07-02 10:22:21 +0100

    Removed urls

--- samples/PointToPoint/src/sender.js
+++ samples/PointToPoint/src/sender.js
@@ -18,7 +18,7 @@ var sendMessages = function () {
 
 var bus = Bus.initialize(function (config) {
     config.queue = "rmessagebus.stomp.pointtopoint.sender";
-    config.url = "http://lonappdev04:15674/stomp"; // Enable stomp adapter using rabbitmq-plugins enable rabbitmq_stomp
+    config.url = "http://localhost:15674/stomp"; // Enable stomp adapter using rabbitmq-plugins enable rabbitmq_stomp
   
     config.queueMappings = {  // Destination to send messages to.  
         "Message1": "rmessagebus.stomp.pointtopoint.consumer",
